f2516967d496121ba156bc195c2b1f6dc44f58dd
Julien Desrosiers <juliend2@gmail.com>
2010-08-24 19:11:01 -0400

    added email validation on creation of the comments

--- lib/comment.rb
+++ lib/comment.rb
@@ -3,6 +3,7 @@ class Comment < ActiveRecord::Base
   validates_presence_of :email
   validates_presence_of :comment
   validates_presence_of :post_id
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
   
   belongs_to :post
   
